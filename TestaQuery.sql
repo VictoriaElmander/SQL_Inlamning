63ef52acaf805cc163b37400e3d907cb834606d6
Victoria Elmander <victoria.elmander@gmail.com>
2025-12-16 18:49:37 +0100

    Updated initial commit

--- TestaQuery.sql
+++ TestaQuery.sql
@@ -1,6 +1,72 @@
 USE AdventureWorks2025
 
+SELECT TOP 5 * FROM Production.ProductCategory
+SELECT TOP 5 * FROM Production.ProductSubcategory
+SELECT * FROM Production.Product
+
+--1: Antal produkter per kategori
+--Affärsfråga: Hur många produkter finns i varje kategori?
+SELECT 
+    COALESCE (pc.Name, 'Saknar kategori') AS Kategori,
+    COUNT(p.ProductID) AS AntalProdukter
+FROM 
+    Production.Product p
+LEFT JOIN Production.ProductSubcategory ps 
+    ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory pc 
+    ON ps.ProductCategoryID = pc.ProductCategoryID
+GROUP BY 
+    COALESCE(pc.Name, 'Saknar kategori')
+ORDER BY 
+    CASE 
+        WHEN COALESCE(pc.Name, 'Saknar kategori') = 'Saknar kategori' THEN 1
+        ELSE 0
+    END,
+    AntalProdukter DESC;
+
+--2: Försäljning per produktkategori
+--Affärsfråga: Vilka produktkategorier genererar mest intäkter?
 
 SELECT TOP 5 * FROM Production.ProductCategory
 SELECT TOP 5 * FROM Production.ProductSubcategory
-SELECT TOP 5 * FROM Production.Product
+SELECT * FROM Production.Product
+SELECT TOP 30 * FROM Sales.SalesOrderDetail WHERE UnitPriceDiscount > 0
+SELECT COUNT(*) AS AntalRader FROM Sales.SalesOrderDetail
+
+--Kontrollera totalsumma
+SELECT SUM(LineTotal) AS TotalLineTotal
+FROM Sales.SalesOrderDetail;
+
+--Kontrollera om någon rad saknar kategori --> ingen rad saknar kategori
+SELECT COUNT(*) AS OrderraderMedSaknadKategori
+FROM Sales.SalesOrderDetail sod
+JOIN Production.Product p ON sod.ProductID = p.ProductID
+LEFT JOIN Production.ProductSubcategory ps ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory pc ON ps.ProductCategoryID = pc.ProductCategoryID
+WHERE pc.ProductCategoryID IS NULL;
+
+--Lägg in i ipynb
+SELECT 
+    COALESCE (pc.Name, 'Saknar kategori') AS Kategori,
+    SUM(LineTotal) AS SummaKategori
+FROM 
+    Sales.SalesOrderDetail sod
+INNER JOIN Production.Product p
+    ON sod.ProductID = p.ProductID
+LEFT JOIN Production.ProductSubcategory ps 
+    ON p.ProductSubcategoryID = ps.ProductSubcategoryID
+LEFT JOIN Production.ProductCategory pc 
+    ON ps.ProductCategoryID = pc.ProductCategoryID
+GROUP BY 
+    COALESCE(pc.Name, 'Saknar kategori')
+ORDER BY 
+    CASE WHEN COALESCE(pc.Name, 'Saknar kategori') = 'Saknar kategori' THEN 1 ELSE 0 END,
+    SummaKategori DESC;
+
+
+SELECT TOP 5 * FROM Sales.SalesOrderHeader
+SELECT * FROM Sales.CurrencyRate
+
+SELECT DISTINCT ToCurrencyCode
+FROM Sales.CurrencyRate
+WHERE ToCurrencyCode = 'USD';
